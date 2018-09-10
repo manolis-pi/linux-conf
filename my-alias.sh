f8ea87d3e704802ca0aabe6616593dd927a93dee
manolis-pi <manolis-pi@users.noreply.github.com>
2018-09-10 23:45:49 +0300

    Create my-alias.sh

---
+++ my-alias.sh
@@ -0,0 +1,19 @@
+alias ls='ls -F'
+alias ll='ls -l'
+alias mv='mv -i'
+alias rm='rm -i'
+alias cp='cp -i'
+alias p='more'
+alias cls='clear'
+alias sudo='sudo ' #notice the extra space at the end
+
+if [ -x /usr/bin/dircolors ]; then
+    test -r ~/.dircolors && eval "$(dircolors -b ~/.dircolors)" || eval "$(dircolors -b)"
+    alias ls='ls -F --color=auto'
+    #alias dir='dir --color=auto'
+    #alias vdir='vdir --color=auto'
+
+    alias grep='grep --color=auto'
+    alias fgrep='fgrep --color=auto'
+    alias egrep='egrep --color=auto'
+fi
